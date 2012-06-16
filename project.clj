7167e38b4dc0e0948ccfb2c835004a297ce71bd9
Dima Sabanin <sdmitry@gmail.com>
2012-06-16 23:42:56 +0800

    added lein-midje as a dev-dependency

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
   :dev-dependencies [[midje "1.4.0"]
                      [com.stuartsierra/lazytest "1.2.3"]
                      [lein-marginalia "0.7.1"]
-                     [lein-clojars "0.9.0"]]
+                     [lein-clojars "0.9.0"]
+                     [lein-midje "1.0.10"]]
   :repositories {"tmatesoft" "http://maven.tmatesoft.com/content/repositories/releases/"
                  "stuartsierra-releases" "http://stuartsierra.com/maven2"})
