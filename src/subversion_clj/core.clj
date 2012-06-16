f7872afcdc6fcf26c0a87cc802a44466e453717d
Dima Sabanin <sdmitry@gmail.com>
2012-06-16 11:59:44 +0800

    empty leiningen project

---
+++ src/subversion_clj/core.clj
@@ -0,0 +1,1 @@
+(ns subversion-clj.core)
---
+++ test/subversion_clj/test/core.clj
@@ -0,0 +1,6 @@
+(ns subversion-clj.test.core
+  (:use [subversion-clj.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
