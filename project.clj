c7763a730492e17fd2ffe22a9cc6a057c3dcd455
Dima Sabanin <sdmitry@gmail.com>
2012-06-16 15:58:21 +0800

    added lein-clojars dev-dependency

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
                  [org.tmatesoft.svnkit/svnkit "1.7.4"]]
   :dev-dependencies [[midje "1.4.0"]
                      [com.stuartsierra/lazytest "1.2.3"]
-                     [lein-marginalia "0.7.1"]]
+                     [lein-marginalia "0.7.1"]
+                     [lein-clojars "0.9.0"]]
   :repositories {"tmatesoft" "http://maven.tmatesoft.com/content/repositories/releases/"
                  "stuartsierra-releases" "http://stuartsierra.com/maven2"})
