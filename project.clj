39c838beb399f0f8845348844a76201d73ea19f3
Dima Sabanin <sdmitry@gmail.com>
2013-03-28 19:55:05 -0400

    removing clojure 1.3 and updating svnkit to 1.7.8

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject subversion-clj "0.1"
   :description "SVNKit based Subversion API for Clojure"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.tmatesoft.svnkit/svnkit "1.7.4"]]
+  :dependencies [[org.tmatesoft.svnkit/svnkit "1.7.8"]]
   :dev-dependencies [[midje "1.4.0"]
                      [com.stuartsierra/lazytest "1.2.3"]
                      [lein-marginalia "0.7.1"]
