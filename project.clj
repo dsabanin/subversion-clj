371f2dc6837881c993c04d07ccb7a2f381012b25
Dima Sabanin <sdmitry@gmail.com>
2013-03-28 20:16:23 -0400

    New style lein dev dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject subversion-clj "0.1"
+(defproject subversion-clj "0.1.1"
   :description "SVNKit based Subversion API for Clojure"
-  :dependencies [[org.tmatesoft.svnkit/svnkit "1.7.8"]]
-  :dev-dependencies [[midje "1.4.0"]
-                     [com.stuartsierra/lazytest "1.2.3"]
-                     [lein-marginalia "0.7.1"]
-                     [lein-clojars "0.9.0"]
-                     [lein-midje "1.0.10"]]
+  :dependencies [[org.tmatesoft.svnkit/svnkit "1.7.8"]
+                 [org.clojure/clojure "1.5.1"]]
+  :profiles {:dev {:dependencies [[midje "1.5.1"]
+                                  [com.stuartsierra/lazytest "1.2.3"]
+                                  [lein-marginalia "0.7.1"]
+                                  [lein-midje "3.0.1"]]}}
   :repositories {"tmatesoft" "http://maven.tmatesoft.com/content/repositories/releases/"
                  "stuartsierra-releases" "http://stuartsierra.com/maven2"})
