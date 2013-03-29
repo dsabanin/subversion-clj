456b44377681d4be70980e53743766c9ba0bea1d
Dima Sabanin <sdmitry@gmail.com>
2013-03-28 20:39:28 -0400

    Trying to fix leiningen deps

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
                  [org.clojure/clojure "1.5.1"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]
                                   [com.stuartsierra/lazytest "1.2.3"]
-                                  [lein-marginalia "0.7.1"]
-                                  [lein-midje "3.0.1"]]}}
+                                  [lein-marginalia "0.7.1"]]
+                   :plugins [[lein-midje "3.0.1"]]}}
   :repositories {"tmatesoft" "http://maven.tmatesoft.com/content/repositories/releases/"
                  "stuartsierra-releases" "http://stuartsierra.com/maven2"})
