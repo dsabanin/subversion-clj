cf76bba073271d146e57df84fb59ed01b6f8aa00
Dima Sabanin <sdmitry@gmail.com>
2013-04-16 10:28:08 -0400

    Bumping a version to 0.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject subversion-clj "0.2.0"
+(defproject subversion-clj "0.2.1"
   :description "SVNKit based Subversion API for Clojure"
   :dependencies [[org.tmatesoft.svnkit/svnkit "1.7.9"]
                  [org.clojure/clojure "1.5.1"]]
