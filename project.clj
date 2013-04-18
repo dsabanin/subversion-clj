42265b00621998644bf89875fea277109d2b5d5b
Dima Sabanin <sdmitry@gmail.com>
2013-04-18 11:08:57 -0400

    missed a dependency

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject subversion-clj "0.2.2"
   :description "SVNKit based Subversion API for Clojure"
   :dependencies [[org.tmatesoft.svnkit/svnkit "1.7.9"]
-                 [org.clojure/clojure "1.5.1"]]
+                 [org.clojure/clojure "1.5.1"]
+                 [org.apache.commons/commons-io "1.3.2"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]
                                   [com.stuartsierra/lazytest "1.2.3"]
                                   [lein-marginalia "0.7.1"]]
